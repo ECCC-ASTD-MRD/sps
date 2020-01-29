8cddc74a82747e024082df3b0fbd92271714acb8
Stephane Chamberland <sch001@science.gc.ca>
2020-01-29 23:33:40 +0000

    Postpone rpnphy+sps+scm changes to a11

--- src/base/phys_itf_mod.F90
+++ src/base/phys_itf_mod.F90
@@ -159,7 +159,6 @@ contains
       real :: std_p_prof(MAX_LEVELS)
       logical :: Lvl_stag_L,Lvl_Tlift_L
       character(len=128) :: msg_S
-      character(len=WB_MAXSTRINGLENGTH) :: input_type_S
       !---------------------------------------------------------------------
       call msg(MSG_DEBUG,'[BEGIN] phys_init')
       F_istat = RMN_ERR
@@ -217,14 +216,6 @@ contains
 !!$!     Re-define P_out_moyhr in units of hours, rather than in timesteps
 !!$      P_out_moyhr = ( P_out_moyhr * Cstv_dt_8 ) / 3600.
 
-      err(13) = wb_get('phy/input_type', input_type_S)
-      if (WB_IS_OK(err(13))) then
-         call msg(MSG_DEBUG, 'phy/input_type: '//input_type_S)
-         if (input_type_S /= 'BLOC') then
-            call msg(MSG_ERROR, 'input_type = BLOC required')
-            err(14) = RMN_ERR
-         endif
-      endif
 
       F_istat = minval(err(:))
       F_istat = min(priv_consis(),F_istat)
